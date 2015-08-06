f143bf4e292f43404ccc1f1ea2a44883697eda0a
Flavius12 <info@evolsoft.tk>
2015-08-06 14:44:42 +0200

    Create ffplugin_myalertsformatter.php

---
+++ inc/plugins/ffplugin/ffplugin_myalertsformatter.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * Followers/Following Plugin for MyBB (v1.0) by EvolSoft
+ * Developer: EvolSoft
+ * Website: http://www.evolsoft.tk
+ * Date: 06/08/2015 01:44 PM (UTC)
+ * Copyright & License: (C) 2015 EvolSoft
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class FFPluginMyAlertsFormatter extends MybbStuff_MyAlerts_Formatter_AbstractFormatter {
+    /**
+     * Format an alert into it's output string to be used in both the main alerts listing page and the popup.
+     *
+     * @param MybbStuff_MyAlerts_Entity_Alert $alert The alert to format.
+     *
+     * @return string The formatted alert string.
+     */
+    public function formatAlert(MybbStuff_MyAlerts_Entity_Alert $alert, array $outputAlert){
+        return $outputAlert['from_user'] . " is now following you.";
+    }
+	
+	 /**
+     * Init function called before running formatAlert(). Used to load language files and initialize other required
+     * resources.
+     *
+     * @return void
+     */
+    public function init(){
+    }
+
+    /**
+     * Build a link to an alert's content so that the system can redirect to it.
+     *
+     * @param MybbStuff_MyAlerts_Entity_Alert $alert The alert to build the link for.
+     *
+     * @return string The built alert, preferably an absolute link.
+     */
+    public function buildShowLink(MybbStuff_MyAlerts_Entity_Alert $alert){
+        return get_profile_link($alert->getFromUserId());
+    }
+}
+?>
