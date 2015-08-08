52ff5d0f2b77567d02b27b44effcceb14f15a589
Flavius12 <info@evolsoft.tk>
2015-08-08 17:33:36 +0200

    Update ffactions.php

--- ffactions.php
+++ ffactions.php
@@ -4,7 +4,7 @@
  * Followers/Following Plugin for MyBB (v1.0) by EvolSoft
  * Developer: EvolSoft
  * Website: http://www.evolsoft.tk
- * Date: 06/08/2015 10:01 AM (UTC)
+ * Date: 08/08/2015 04:12 AM (UTC)
  * Copyright & License: (C) 2015 EvolSoft
  * 
  * This program is free software: you can redistribute it and/or modify
@@ -30,73 +30,83 @@ if($mybb->user['uid'] == 0){
 	error_no_permission();
 }
 
-if(isset($_GET['action']) && isset($_GET['uid'])){
-	if(strtolower($_GET['action']) == "follow"){
-		if(get_user($_GET['uid']) == null){
-			if(isset($_GET['ajax'])){
+if(isset($mybb->input['action']) && isset($mybb->input['uid'])){
+	if(strtolower($mybb->input['action']) == "follow"){
+		if(get_user($mybb->input['uid']) == null){
+			if(isset($mybb->input['ajax'])){
 				header("Content-type: application/json;");
 				echo json_encode(array("status" => "false", "followers" => 0));
 			}else{
 				error("Can't add this user to your followers list: User not found.");
 			}
 		}else{
-			if(mysqli_num_rows($db->query("SELECT * FROM " . TABLE_PREFIX . "ffplugin WHERE following='" . $mybb->user['uid'] . "' AND follower='" . $_GET['uid'] . "'")) == 0){
-				$db->query("INSERT INTO " . TABLE_PREFIX . "ffplugin (following, follower) VALUES ('" . $mybb->user['uid'] . "', '" . $_GET['uid'] . "')");
+			if(mysqli_num_rows($db->query("SELECT * FROM " . TABLE_PREFIX . "ffplugin WHERE following='" . $mybb->user['uid'] . "' AND follower='" . $mybb->input['uid'] . "'")) == 0){
+				$db->query("INSERT INTO " . TABLE_PREFIX . "ffplugin (following, follower) VALUES ('" . $mybb->user['uid'] . "', '" . $mybb->input['uid'] . "')");
 				if($mybb->settings['ffplugin_em'] == 1){
 					if(function_exists("myalerts_info")){
 						$myalertsinfo = myalerts_info();
 						if($myalertsinfo['version'] >= "2.0.0"){
 							$alertType = MybbStuff_MyAlerts_AlertTypeManager::getInstance()->getByCode('ffplugin_myalerts');
 							if ($alertType != null && $alertType->getEnabled()) {
-								$alert = new MybbStuff_MyAlerts_Entity_Alert($_GET['uid'], $alertType, 0);
+								$alert = new MybbStuff_MyAlerts_Entity_Alert($mybb->input['uid'], $alertType, 0);
 								MybbStuff_MyAlerts_AlertManager::getInstance()->addAlert($alert);
 							}
 						}
 					}
 				}
-				if(isset($_GET['ajax'])){
+				if(isset($mybb->input['ajax'])){
 					header("Content-type: application/json;");
-					echo json_encode(array("status" => "true", "followers" => countf($_GET['uid'], false)));
+					echo json_encode(array("status" => "true", "followers" => countf($mybb->input['uid'], false)));
 				}else{
-					redirect("member.php?action=profile&uid=" . $_GET['uid'], "You are now following " . getName($_GET['uid']) . ".");
+					redirect("member.php?action=profile&uid=" . $mybb->input['uid'], "You are now following " . getName($mybb->input['uid']) . ".");
 				}
 			}else{
-				if(isset($_GET['ajax'])){
+				if(isset($mybb->input['ajax'])){
 					header("Content-type: application/json;");
-					echo json_encode(array("status" => "true", "followers" => countf($_GET['uid'], false)));
+					echo json_encode(array("status" => "true", "followers" => countf($mybb->input['uid'], false)));
 				}else{
 					error("You are already following this user.");
 				}
 			}
 		}
-	}elseif(strtolower($_GET['action']) == "unfollow"){
-		if(mysqli_num_rows($db->query("SELECT * FROM " . TABLE_PREFIX . "ffplugin WHERE following='" . $mybb->user['uid'] . "' AND follower='" . $_GET['uid'] . "'")) == 0){
-			if(isset($_GET['ajax'])){
+	}elseif(strtolower($mybb->input['action']) == "unfollow"){
+		if(mysqli_num_rows($db->query("SELECT * FROM " . TABLE_PREFIX . "ffplugin WHERE following='" . $mybb->user['uid'] . "' AND follower='" . $mybb->input['uid'] . "'")) == 0){
+			if(isset($mybb->input['ajax'])){
 				header("Content-type: application/json;");
-				echo json_encode(array("status" => "false", "followers" => countf($_GET['uid'], false)));
+				echo json_encode(array("status" => "false", "followers" => countf($mybb->input['uid'], false)));
 			}else{
 				error("You are not following this user.");
 			}
 		}else{
-			$db->query("DELETE FROM " . TABLE_PREFIX . "ffplugin WHERE following='" . $mybb->user['uid'] . "' AND follower='" . $_GET['uid'] . "'");
-			if(isset($_GET['ajax'])){
+			$db->query("DELETE FROM " . TABLE_PREFIX . "ffplugin WHERE following='" . $mybb->user['uid'] . "' AND follower='" . $mybb->input['uid'] . "'");
+			if(isset($mybb->input['ajax'])){
 				header("Content-type: application/json;");
-				echo json_encode(array("status" => "false", "followers" => countf($_GET['uid'], false)));
+				echo json_encode(array("status" => "false", "followers" => countf($mybb->input['uid'], false)));
 			}else{
-				redirect("member.php?action=profile&uid=" . $_GET['uid'], "You are not following " . getName($_GET['uid']) . " anymore.");
+				redirect("member.php?action=profile&uid=" . $mybb->input['uid'], "You are not following " . getName($mybb->input['uid']) . " anymore.");
 			}
 		}
-	}elseif(strtolower($_GET['action']) == "showfollowinglist"){
-		if(get_user($_GET['uid']) == null){
-			drawModal("Following", 500, "<tr><td class=\"trow1\" colspan=\"2\"><em>Can't get the following list of this user: User not found.</em></td></tr>");
+	}elseif(strtolower($mybb->input['action']) == "showfollowinglist"){
+		if(isset($mybb->input['page'])){
+			$page = $mybb->input['page'];
 		}else{
-			drawModal("Following", 500, processModalList($_GET['uid'], true));
+			$page = 0;
 		}
-	}elseif(strtolower($_GET['action']) == "showfollowerslist"){
-		if(get_user($_GET['uid']) == null){
-			drawModal("Followers", 500, "<tr><td class=\"trow1\" colspan=\"2\"><em>Can't get the followers list of this user: User not found.</em></td></tr>");
+		if(get_user($mybb->input['uid']) == null){
+			drawModal("Following (Page " . ($page + 1) . ")", 500, "<tr><td class=\"trow1\" colspan=\"2\"><em>Can't get the following list of this user: User not found.</em></td></tr>");
 		}else{
-			drawModal("Followers", 500, processModalList($_GET['uid'], false));
+			drawModal("Following (Page " . ($page + 1) . ")", 500, processModalList($mybb->input['uid'], true, $page));
+		}
+	}elseif(strtolower($mybb->input['action']) == "showfollowerslist"){
+		if(isset($mybb->input['page'])){
+			$page = $mybb->input['page'];
+		}else{
+			$page = 0;
+		}
+		if(get_user($mybb->input['uid']) == null){
+			drawModal("Followers (Page " . ($page + 1) . ")", 500, "<tr><td class=\"trow1\" colspan=\"2\"><em>Can't get the followers list of this user: User not found.</em></td></tr>");
+		}else{
+			drawModal("Followers (Page " . ($page + 1) . ")", 500, processModalList($mybb->input['uid'], false, $page));
 		}
 	}else{
 		error("You are trying to perform an invalid action.");
